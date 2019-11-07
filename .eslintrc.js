6bd60aa8f21bf183b6863aef6bd874a805474e8d
Junior Dias <juniordias_@live.com>
2019-11-07 11:05:44 -0300

    update project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,33 @@
 module.exports = {
   env: {
     es6: true,
-    node: true,
+    browser: true,
   },
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
+  extends: [
+    'airbnb-base','prettier', 'prettier/react'
+  ],
+  plugins: ['prettier', 'react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
+    ecmaFeatures:{
+      jsx:true,
+    },
     ecmaVersion: 2018,
     sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "error",
-    "class-methods-use-this": "off",
-    "no-param-reassign": "off",
-    "camelcase": "off",
-    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+    "prettier/prettier" :"error",
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+
+
   },
 };
