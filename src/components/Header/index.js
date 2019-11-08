7aeec260677ac3d9165e225c041319b05a3f16c3
Junior Dias <juniordias_@live.com>
2019-11-08 11:15:31 -0300

    create components Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import {MdShoppingBasket} from 'react-icons/md';
+
+import { Container, Cart, Logo } from './styles';
+
+ import logo from '../../assets/images/logo.svg';
+
+export default function Header() {
+  return (
+   <Container>
+     <Link to="/">
+       <Logo to="/">
+     <img src={logo} alt="SquadsShoes"  />
+     </Logo>
+     </Link>
+     <Cart to="/car">
+       <div>
+         <strong>Meu Carrinho</strong>
+         <span>3 Itens</span>
+         </div>
+         <MdShoppingBasket size={36} color="#fff" />
+       </Cart>
+     </Container>
+  );
+}
