f5bcec2c786a7ae9e9c7ec7a822a34815aad1553
Junior Dias <juniordias_@live.com>
2019-11-11 13:49:42 -0300

    consulting stock in cart with Redux Saga

--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -18,10 +18,19 @@
    };
   }
 
-  export function updateAmount(id, amount) {
+  export function updateAmountRequest(id, amount) {
     return {
-      type: '@cart/UPDATE_AMOUNT',
+      type: '@cart/UPDATE_AMOUNT_REQUEST',
+      id,
+      amount,
+    }
+  }
+
+  export function updateAmountSuccess(id, amount) {
+    return {
+      type: '@cart/UPDATE_AMOUNT_SUCCESS',
       id,
       amount,
     };
   }
+
--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -4,7 +4,7 @@ import {toast} from 'react-toastify';
 import api from '../../../services/api';
 import { formatPrice} from '../../../util/format';
 
-import { addToCartSuccess, updateAmount} from './actions';
+import { addToCartSuccess, updateAmountSuccess} from './actions';
 
 function* addToCart({id}) {
   const productExists = yield select(
@@ -24,7 +24,7 @@ function* addToCart({id}) {
   }
 
   if(productExists) {
-    yield put(updateAmount(id, amount));
+    yield put(updateAmountSuccess(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
 
@@ -39,8 +39,24 @@ function* addToCart({id}) {
 
 }
 
+function* updateAmount({ id, amount}) {
+  if (amount <= 0) return;
+
+
+  const stock = yield call(api.get, `/stock/${id}`);
+  const stockAmount = stock.data.amount;
+
+  if(amount > stockAmount) {
+    toast.error('Quantidade solicitada fora de estoque');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+}
+
 export default all([
  takeLatest('@cart/ADD_REQUEST', addToCart), //Ouvir actions disparada
+ takeLatest('@cart/UPDATE_AMOUNT_REQUEST', updateAmount),
 ]);
 
 
