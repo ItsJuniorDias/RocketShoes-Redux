3ee1e8f68c01b32bcfc769b3653306ebe7ba1c01
Junior Dias <juniordias_@live.com>
2019-11-11 09:40:04 -0300

    changing item quantity

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -7,7 +7,15 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { Container,ProductTable,Total } from './styles';
 
-function Cart({ cart, removeFromCart }) {
+function Cart({ cart, removeFromCart, updateAmount }) {
+ function increment(product) {
+     updateAmount(product.id, product.amount + 1 );
+ }
+
+ function decrement(product) {
+    updateAmount(product.id, product.amount - 1);
+ }
+
   return (
     <Container>
       <ProductTable>
@@ -34,11 +42,11 @@ function Cart({ cart, removeFromCart }) {
               </td>
               <td>
                 <div>
-                <button type="button">
+                <button type="button" onClick={() => decrement(product)}>
                 <MdRemoveCircleOutline size={20} color="#404040"/>
                 </button>
                 <input type="number" readOnly value={product.amount} />
-                <button type="button">
+                <button type="button" onClick={() => increment(product)}>
                 <MdAddCircleOutline size={20} color="#404040"/>
                 </button>
                 </div>
--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -10,3 +10,11 @@
     id,
    };
   }
+
+  export function updateAmount(id, amount) {
+    return {
+      type: '@cart/UPDATE_AMOUNT',
+      id,
+      amount,
+    };
+  }
